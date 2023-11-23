d47ea48ab3b757370335a32437b7c9ca29404eb5
Rocio <rbrueda8@gmail.com>
2023-11-23 02:56:22 -0500

    Filter Data & Fixed Praw Issue

--- ibm.py
+++ ibm.py
@@ -1,20 +1,35 @@
 import json
 from ibm_watson import NaturalLanguageUnderstandingV1
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-from ibm_watson.natural_language_understanding_v1 \
-    import Features, CategoriesOptions
+from ibm_watson.natural_language_understanding_v1 import Features,CategoriesOptions,EmotionOptions,KeywordsOptions
+import pandas as pd
+import numpy as np
+from datetime import datetime
+
+#opens the csv file as a dataframe
+df = pd.read_csv('reddit_result.csv')
+
 
 #find api key and enter it inside the curly braces
-authenticator = IAMAuthenticator('{apikey}')
+authenticator = IAMAuthenticator('KmmH50mQGUkBkAZSb80T6vrWaPt-pJRie0oPXuwJNNSO')
 natural_language_understanding = NaturalLanguageUnderstandingV1(
     version='2022-04-07',
     authenticator=authenticator
 )
 
-natural_language_understanding.set_service_url('{url}')
+#get content in selftext -- for fture we will extract timestamp and comments content
+text = df.loc[3,"selftext"]
+#get all content in this columns and set to a new df called df2
+df2 = df['selftext'].dropna(how = 'all')
+df_text = df2.to_string()
+print(df_text)
+
+
+
 
 response = natural_language_understanding.analyze(
-    url='www.ibm.com',
-    features=Features(categories=CategoriesOptions(limit=3))).get_result()
+    # url='www.ibm.com',
+    text = df_text,
+    features=Features(keywords=KeywordsOptions(sentiment=True,emotion=True,limit=5))).get_result()
 
 print(json.dumps(response, indent=2))
--- reddit_api.py
+++ reddit_api.py
@@ -1,124 +1,122 @@
-import requests
-import streamlit as st
+import praw
+from datetime import datetime, timedelta
 import pandas as pd
-import json
-# doing a db libraries
-# import sqlalchemy
-# from sqlalchemy import create_engine, text
-import os
+#preprocessing portion of code -- use re to detect regular expressions (in this case emoticons)
+import re
+import csv
+import chardet
 
-#these are my ids for the reddit api
-CLIENT_ID = 'kg6ykpAJcVRKtOLO8GbZ2g'
-SECRET_KEY = 'rdk29dYcjhdm01Wn45VxI2g_HVhuZg'
 
-#print(sqlalchemy.__version__)
-print(os.getcwd())
+print(0)
+credentials = []
+
 
 with open('credentials.txt', 'r') as f:
-    pw = f.read()
+    for content in f:
+        #add to array of content
+        credentials.append(content.strip())
 
+
+#information with account and api information
+
+#these are my ids for the reddit api
+CLIENT_ID = credentials[0]
+SECRET_KEY = credentials[1]
+
+#print(sqlalchemy.__version__)
 
 #db credentials
-usr = 'root'
-pwd = pw
-host = '127.0.0.1'
-port = 3306
-dbName = 'redditdb'
-tableName = 'reddit_credentials'
+# usr = 'root'
+# pwd = credentials[2] #password to enter database -- wont need until later
+# host = '127.0.0.1'
+# port = 3306
+# dbName = 'redditdb'
+# tableName = 'reddit_credentials'
+
+#to get comments
+reddit = praw.Reddit(
+    client_id = credentials[0],
+    client_secret=credentials[1],
+    password=credentials[2],
+    user_agent=credentials[4],
+    username=credentials[3]
+
+)
 
 
+# Define the terms to search for and the subreddit
+search_terms = ['gpt', 'ai', 'generative ai', 'chatgpt']
+subreddit = 'all'  # Or specify a subreddit
+print(1)
+
+# Calculate date range for "yesterday" (or adjust to the specific day you're fetching for)
+yesterday = datetime.utcnow() - timedelta(days=60)
+date_limit = yesterday.strftime('%Y%m%d')
+print(date_limit)
+
+print(2)
+# Fetch posts
+posts_data = []
+for term in search_terms:
+    for submission in reddit.subreddit(subreddit).search(term, time_filter='day', limit=20):
+        submission.comments.replace_more(limit=100)
+        print(submission)
+        #if datetime.utcfromtimestamp(submission.created_utc).strftime('%Y%m%d') == date_limit:
+        posts_data.append({
+            'title': submission.title,
+            'description': submission.selftext,
+            'comments': [comment.body for comment in submission.comments.list()],
+            'subreddit': submission.subreddit.display_name,
+            'karma': submission.score,
+            'url': submission.url,
+            'posted_date': datetime.utcfromtimestamp(submission.created_utc)
+        })
+        #print post_data -- this was just to track if it was working
+        #before it was making too many requests causing a 429 error, which made it print nothing
+        print(posts_data)
+
+# Convert to DataFrame
+df = pd.DataFrame(posts_data)
+
+# Save DataFrame to a CSV file or database
+df.to_csv(f'reddit_results.csv', index=False)
 
 
-auth = requests.auth.HTTPBasicAuth(CLIENT_ID, SECRET_KEY)
+#preprocessing part of code
+#code by: https://stackoverflow.com/questions/33404752/removing-emojis-from-a-string-in-python
+emoji_pattern = re.compile("["
+                           u"\U0001F600-\U0001F64F"  # emoticons
+                           u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                           u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                           u"\U0001F700-\U0001F77F"  # alchemical symbols
+                           u"\U0001F780-\U0001F7FF"  # Geometric Shapes Extended
+                           u"\U0001F800-\U0001F8FF"  # Supplemental Arrows-C
+                           u"\U0001F900-\U0001F9FF"  # Supplemental Symbols and Pictographs
+                           u"\U0001FA00-\U0001FA6F"  # Chess Symbols
+                           u"\U0001FA70-\U0001FAFF"  # Symbols and Pictographs Extended-A
+                           u"\U00002702-\U000027B0"  # Dingbats
+                           u"\U000024C2-\U0001F251" 
+                           "]+", flags=re.UNICODE)
 
- #specifying a dictionary in which we are going to go in with a password
-data = {
-    'grant_type' : 'password',
-    'username' : 'flamingolover4',
-    'password' : pw
-}
+# Detect the encoding of the file
+with open('reddit_results.csv', 'rb') as infile:
+    rawdata = infile.read()
+    result = chardet.detect(rawdata)
 
-#identify the version of yoyr api
-headers = {'User-Agent' : 'MyAPI/0.0.1'}
+# Use the detected encoding to read the file and write to the output file with 'utf-8' encoding
+with open('reddit_results.csv', 'r', encoding=result['encoding']) as infile, open('reddit_results_filtered.csv', 'w', newline='', encoding='utf-8') as outfile:
+    reader = csv.reader(infile)
+    writer = csv.writer(outfile)
 
-#innclude all our daa for the request
-res = requests.post('https://www.reddit.com/api/v1/access_token', 
-                     auth=auth, data=data, headers=headers)
+    for row in reader:
+        print(row)
+        #replace the pattern in all columns for csv file
+        modified_row = [re.sub(emoji_pattern, " ", value) for value in row]
 
-TOKEN = res.json()['access_token']
-print(TOKEN)
+        #two modifiable conditions adding later: 
+        # 1. Convert comments from a list of strings to one string
+        # 2. 
 
-#this access token is something we need to add to ur header
-# at this point we can access any token we would like
-headers['Authorization'] = f'bearer {TOKEN}'
+        writer.writerow(modified_row)
 
-print(headers)
-
-#this is just ot see if the api is working, will get all the information in your account
-print(requests.get("https://oauth.reddit.com/api/v1/me", headers=headers).json())
-
-#allows to get max requests
-res = requests.get("https://oauth.reddit.com/r/ChatGPT", headers=headers, params={'limit':'1000'})
-#, params={'limit':'100', 'after' : 't3_17evdt8'}
-#get data after this post
-
-#prints the results in json format
-print(res.json())
-
-#create a dataframe that filters certain information via specfied columns
-df = pd.DataFrame(columns=['subreddit', 'title', 'selftext', 'upvote_ratio', 'ups', 'downs', 'score'])
-
-#goes through each post per results in json format
-for post in res.json()['data']['children']:
-
-    print(post['data']) #just extracts the data in the post
-
-    #creates a dataframe with these columns of data
-    df = pd.concat([df, pd.DataFrame.from_records([{ 'subreddit' : post['data']['subreddit'], 'title' : post['data']['title'], 'selftext' : post['data']['selftext'], 'upvote_ratio' : post['data']['upvote_ratio'], 'ups' : post['data']['upvote_ratio'], 'downs' : post['data']['downs'], 'score' : post['data']['score']}])], ignore_index=True)
-
-#see it visually on a dataframe on streamlit app
-st.write(df)
-
-#save content from df to csv
-df.to_csv("reddit_result.csv", index=False)
-
-print(post['kind'] + '_' + post['data']['id'])
-
-#To do: get all reddit post about chat gpt and short data into a separate json file
-#later: organize data into a database or dataframe and make a call to the ibm api ot get infomation about sentiment analysis, when we can make a call to power bi or any other tool to make a graph for us
-# before power bi: make a graph on streamlit to see results
-
-#goal: find a way to access comments in reddit
-
-
-#sql stuff -- do later for make a dataset
-# #create an engine object
-# engine = create_engine(
-#     f"mysql+mysqldb://{usr}:{pwd}@{host}:{port}/{dbName}",
-#     echo=True,
-#     future=True)
-
-# #create table
-# df.to_sql(name=tableName, con=engine, if_exists='replace')
-
-# with engine.connect() as conn:
-#     result = conn.execute(text("DESCRIBE tableName;"))
-#     for row in result:
-#         print(row)
     
-# with engine.connect() as conn:
-#     result = conn.execute(text("SELECT * FROM tableName"))
-#     for row in result:
-#         print(f"""
-#             index: {row.index}
-#             subreddit: {row.subreddit}
-#             title: {row.title}
-#             selftext: {row.selftext}
-#             upvote_ratio: {row.count}
-#             ups: {row.count}
-#             downs: {row.}
-#             score: {row.}
-
-#               """)
-
-
